252f5dfc9fadcb924808376b785ca4bb0f68b1c3
matheoenee <messagerie.matheo@gmail.com>
2023-11-28 17:37:43 +0100

    RSA questions

--- rsa.py
+++ rsa.py
@@ -1,1 +1,9 @@
 # file for generate RSA keys, encryption function and decryption function
+
+# how to generate propre p and q ? (see https://fr.wikipedia.org/wiki/Chiffrement_RSA)
+
+# fast exponentiation algorithm (to compute (m^e mod n) and (c^d mod n))
+
+# encryption function (how to manage the fact the m > n? How to "split" m?)
+
+# decryption function
