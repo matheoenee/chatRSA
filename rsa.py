37bc285defec839d0730d407980033308d5208c4
tonyfrg <anthony.fraga2001@gmail.com>
2023-12-28 12:03:44 +0100

    pour récup la dernière version

--- rsa.py
+++ rsa.py
@@ -27,37 +27,26 @@ def lpowmod(x, y, n):
         y = y//2
     return result
 
-#générateur d'entier premier
 def prime_generator(lenght=1024):
-    #étape 1 : liste de 1024 chiffres
     A = random.choices(range(0,10), k=lenght)
-    #pour changer le dernier nombre car il appartenait à [1,3,7,9]
     n1_choice = range(0,10)
-    #n0 permet d'éviter les cas pair et multiple de 5
     n0_choice = [1,3,7,9]
     while True:
-        #on récupère les 1023 derniers chiffres
         A = A[1:]
-        #gestion du cas où le premier chiffre est 0
         if A[0] == '0':
             A[0] = random.choice(range(1,10))
-        #changement du dernier chiffre, puis ajout pour atteindre 1024
         A[-1] = random.choice(n1_choice)
         A.append(random.choice(n0_choice))
 
-        a=''
-        #a est le nombre test en str
+        a = ''
         a = ''.join(map(str,A))
-
-        #p est un nombre a 1024 chiffres
         p = int(a)
 
-        #vérifions si p est premier
         command = 'openssl prime '
         r = subprocess.run(command + a, shell=True,stdout=subprocess.PIPE)
         result = r.stdout
 
-        match = re.search(rb'not', result) #return True si 'not' apparrait dans le message
+        match = re.search(rb'not', result)
         if not match:
             return p
 
@@ -71,7 +60,6 @@ def RSA_key():
     # d=e⁻¹ mod phi_n
     d = modinv(e, phi_n)
     if not d:
-        #this case will not appear as long as e is Fermat number
         return None
     return n, d, e
 
